3b35b44740a1516ac0e9e7d7643bf759c631388e
Micalgenus <micalgenus@gmail.com>
2019-03-29 02:31:17 +0900

    Remove Query test

--- src/schemas/resolvers/Query.spec.ts
+++ src/schemas/resolvers/Query.spec.ts
@@ -1,7 +1,40 @@
-import { expect } from 'chai';
+// import { assert } from 'chai';
+// import { verify } from '@/controllers/auth';
 
-describe('GraphQL Query', () => {
-  it('Mocha test', () => {
-    expect(0).to.equal(0);
-  });
-});
+// const Query = require('./Query');
+// const Mutation = require('./Mutation');
+
+// /* Requirement test */
+// require('./Mutation.spec');
+
+// describe('GraphQL Query', function() {
+//   let token = null;
+//   let user = null;
+
+//   before(async () => {
+//     token = await Mutation.login(null, { user: 'tester', password: 'test1234' });
+//     user = await verify(token);
+//   });
+
+//   describe('me', function() {
+//     it('get user info', async function() {
+//       const userInfo = await Query.me(null, null, { user });
+//       assert.deepEqual(userInfo, { BIRTH: null, BLOCK: 0, EMAIL: 'tester@gmail.com', GENDER: null, ID: 'tester', PHONE: null, USERNAME: 'tester' });
+//     });
+
+//     it('get not exist user info', async function() {
+//       const message = await Query.me(null, null, { user: { _id: -1 } }).catch(err => err.message);
+//       assert.equal(message, 'User not found');
+//     });
+
+//     it('not exist user object', async function() {
+//       const message = await Query.me(null, null, { user: undefined }).catch(err => err.message);
+//       assert.equal(message, 'You are not authenticated!');
+//     });
+
+//     it('not contain primary key in user', async function() {
+//       const message = await Query.me(null, null, { user: {} }).catch(err => err.message);
+//       assert.equal(message, 'Required pk');
+//     });
+//   });
+// });
